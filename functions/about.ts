0f62751259867688a9389751526dafb8c85de94a
Brian Amesbury <bamesbury@sofi.org>
2019-01-12 13:40:18 -0700

    setting the version

---
+++ functions/about.ts
@@ -0,0 +1,2 @@
+const data = {"version":"local","date":0};
+export default data;
---
+++ version.js
@@ -0,0 +1,29 @@
+let exec = require("child_process").exec;
+let fs = require("fs");
+const cmd1 = "git describe --tags";
+const cmd2 = "git status --porcelain";
+
+const data = {
+    version: null,
+    date: new Date().getTime()
+};
+
+exec(cmd1, function (error, stdout1) {
+    exec(cmd2, function (error, stdout2) {
+        if (stdout2.trim().length > 0) {
+            let version = stdout1.trim();
+            if (version.indexOf("-g") !== -1) {
+                let withOutHash = version.substr(0, version.indexOf("-g"));
+                let withOutCommitCount = withOutHash.substr(0, withOutHash.lastIndexOf("-") + 1);
+                let commitCount = withOutHash.substr(withOutHash.lastIndexOf("-") + 1);
+                data.version = withOutCommitCount + (Number.parseInt(commitCount) + 1) + "-SNAPSHOT"
+            } else {
+                data.version = version + "-1-SNAPSHOT";
+            }
+        } else {
+            data.version = stdout1.trim();
+        }
+        fs.writeFileSync("./release-it/src/about.json", JSON.stringify(data));
+        fs.writeFileSync("./functions/about.ts", "const data = " + JSON.stringify(data) + ";\nexport default data;");
+    });
+});
