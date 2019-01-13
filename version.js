6b188e785853aaf90d6b62354dee0e68fb6fe5e8
Brian Amesbury <bamesbury@sofi.org>
2019-01-12 21:26:04 -0700

    correcting version.js 4

--- version.js
+++ version.js
@@ -11,11 +11,11 @@ const data = {
 e(cmd1, function (error, stdout1) {
     e(cmd2, function (error, stdout2) {
         if (stdout2.trim().length > 0) {
-            let version = stdout1.trim();
+            var version = stdout1.trim();
             if (version.indexOf("-g") !== -1) {
-                let withOutHash = version.substr(0, version.indexOf("-g"));
-                let withOutCommitCount = withOutHash.substr(0, withOutHash.lastIndexOf("-") + 1);
-                let commitCount = withOutHash.substr(withOutHash.lastIndexOf("-") + 1);
+                var withOutHash = version.substr(0, version.indexOf("-g"));
+                var withOutCommitCount = withOutHash.substr(0, withOutHash.lastIndexOf("-") + 1);
+                var commitCount = withOutHash.substr(withOutHash.lastIndexOf("-") + 1);
                 data.version = withOutCommitCount + (Number.parseInt(commitCount) + 1) + "-SNAPSHOT"
             } else {
                 data.version = version + "-1-SNAPSHOT";
