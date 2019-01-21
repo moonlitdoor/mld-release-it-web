67166b1915fb167467815c2edfa6d1e0d4f0d3ba
Brian Amesbury <bamesbury@sofi.org>
2019-01-20 23:34:32 -0700

    remove auth user function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -6,9 +6,9 @@ import * as functions from 'firebase-functions';
 // export const helloWorld = functions.https.onRequest((request, response) => {
   // response.send("Hello from Brian!");
 // });
-exports.collect_organizations = functions.auth.user().onCreate((user) => {
-
-});
+// exports.collect_organizations = functions.auth.user().onCreate((user) => {
+//
+// });
 exports.manualTriggers = functions.pubsub.topic('manualTriggers').onPublish((message) => {
     console.log("yea");
     console.log(message);
