e35931ee098f343142925501cb021bdaf7e35c59
Brian Amesbury <bamesbury@sofi.org>
2019-03-14 23:29:46 -0600

    updating the exports

--- functions/src/user/on-user-delete.function.ts
+++ functions/src/user/on-user-delete.function.ts
@@ -4,6 +4,6 @@ import * as functions from "firebase-functions";
 const firestore: FirebaseFirestore.Firestore = admin.initializeApp(functions.config().firebase).firestore();
 const COLLECTION_USERS = "users";
 
-exports.onUserDelete = functions.auth.user().onDelete((user) => {
+export const onUserDelete = functions.auth.user().onDelete((user) => {
     return firestore.collection(COLLECTION_USERS).doc(user.uid).delete();
 });
